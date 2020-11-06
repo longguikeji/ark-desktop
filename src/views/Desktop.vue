ad5cd4676a6162a57552f1f2d44ff4c9852a0de9
wely <wely@longguikeji.com>
2020-11-06 16:51:31 +0800

    init

---
+++ src/views/Desktop.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div class="root">
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data: function () {
+    return {
+    };
+  }
+};
+</script>
+
+<style lang="less">
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-card>
+    <div slot="header">
+      <el-image :src="groupIcon" />
+      <span>{{ groupName }}</span>
+      <el-button style="float: right; padding: 3px 0" type="text">切换地址</el-button>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name:'Login',
+  data(){
+    return {
+      groupIcon:"http://www.longguikeji.com/img/icons/favicon-32x32.png",
+      groupName:"龙归科技"
+    }
+  },
+  mounted(){
+    console.log('asdfsadf')
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
